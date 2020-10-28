f2c41a57b58fb6a1a571315d1c1161158bbfd2e8
Rails Udemy Course <railsudemycourse@gmail.com>
2020-10-28 08:21:47 +0500

    ruby function and hello world ; video 02.015

---
+++ hello_world.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "Welcome to the complete ruby on rails development course"
+hello = "test world"
+puts hello
+
+def say_hello(anything)
+  puts anything
+end
+
+say_hello("test")
+say_hello "hello world"
